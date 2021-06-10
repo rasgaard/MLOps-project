122289ef1758e12a719cd582d95d0e2fed7a8bdb
sofiebach <70587917+sofiebach@users.noreply.github.com>
2021-06-10 10:54:08 +0200

    ny path

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import datasets as ds
 
-df = pd.read_csv('dataset/train.csv')
+df = pd.read_csv('../../data/raw/train.csv')
 df = df.fillna('')
 df = df.rename(columns={'label': 'labels'})
 df['text'] = df['title'] + '\n\n' + df['text']
