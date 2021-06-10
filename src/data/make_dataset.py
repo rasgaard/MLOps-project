c25502a405b15a0b309522c8e9b2db4a2eb72f73
sofiebach <70587917+sofiebach@users.noreply.github.com>
2021-06-10 11:03:56 +0200

    ny make_data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,11 +2,16 @@
 import pandas as pd
 import datasets as ds
 
-df = pd.read_csv('../../data/raw/train.csv')
-df = df.fillna('')
-df = df.rename(columns={'label': 'labels'})
-df['text'] = df['title'] + '\n\n' + df['text']
-df = df.drop(columns=['id', 'title'])
+def dataset():
+    df = pd.read_csv('../../data/raw/train.csv')
+    df = df.fillna('')
+    df = df.rename(columns={'label': 'labels'})
+    df['text'] = df['title'] + '\n\n' + df['text']
+    df = df.drop(columns=['id', 'title'])
 
-dataset = ds.Dataset.from_pandas(df)
-dataset.features['labels'] = ds.ClassLabel(num_classes=2, names=['unreliable', 'reliable'])
+    dataset = ds.Dataset.from_pandas(df)
+    dataset.features['labels'] = ds.ClassLabel(num_classes=2, names=['unreliable', 'reliable'])
+
+    return dataset
+
+dataset = dataset()
