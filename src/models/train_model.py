bcd57fee168b27cf458ce95b08004257adf428f0
Ljoerring <ljoerring@live.dk>
2021-06-14 19:38:54 +0200

    The train script is modified to handle the train/val split that we do in get_encoded_data. Wandb is outcommented. The num_workers argument in the DataLoader is set to 4, meaning that we run on four processors.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,24 +2,23 @@ import torch
 from torch.utils.data import DataLoader
 from transformers import AdamW, RobertaForSequenceClassification
 
-import wandb
+#import wandb
 from src.data.make_dataset import get_encoded_data
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-wandb.init()
+#wandb.init()
 def train(epochs=3, lr=0.003):
     model = RobertaForSequenceClassification.from_pretrained('roberta-base')
     model.to(device)
     model.train()
 
-    wandb.watch(model, log_freq=100)
+    #wandb.watch(model, log_freq=100)
 
     optim = AdamW(model.parameters(), lr=lr)
-    dataset = get_encoded_data(train=True)
-    train_loader = DataLoader(dataset, 4, shuffle=True)
+    encoded_train, encoded_val = get_encoded_data(train=True)
+    train_loader = DataLoader(encoded_train, 16, shuffle=True, num_workers = 4)
 
-""".git/
     for epoch in range(epochs):
         running_loss = 0
         for batch_idx, batch in enumerate(train_loader):
@@ -35,15 +34,14 @@ def train(epochs=3, lr=0.003):
             loss.backward()
             optim.step()
             print(f"Batch {batch_idx+1}/{len(train_loader)}", end='\r')
-            if batch_idx % 40 == 0:
-                wandb.log({"Loss": loss})
+            #if batch_idx % 40 == 0:
+                #wandb.log({"Loss": loss})
 
         print(f"\nEpoch {epoch+1}/{epochs}\tLoss: {running_loss/len(train_loader)}\n")
     # torch.save(model.state_dict(), 'models/checkpoint.pth')
 
     # Should be loadable with RobertaForSequenceClassification.from_pretrained('models/Roberta-fakenews.pt')
     model.save_pretrained('models/Roberta-fakenews.pt') 
-"""
 
 if __name__ == '__main__':
     train()
