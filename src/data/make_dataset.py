6738f17c4c59b42c11b5b67f66a1436b8a7af320
Rasmus Aagaard <raagaard97@gmail.com>
2021-06-21 09:45:51 +0200

    introduced seeds to training

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
 
 
 def read_data():
@@ -9,8 +8,8 @@ def read_data():
     texts, labels = list(df['text'].values), list(df['label'].values)
 
     # train, validation and test split
-    train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, train_size=0.70, test_size=0.30, shuffle=False)
-    test_texts, val_texts, test_labels, val_labels = train_test_split(test_texts, test_labels, train_size=0.5, test_size=0.5, shuffle=False)
+    train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, train_size=0.70, test_size=0.30, shuffle=True)
+    test_texts, val_texts, test_labels, val_labels = train_test_split(test_texts, test_labels, train_size=0.5, test_size=0.5, shuffle=True)
 
     return train_texts, val_texts, test_texts, train_labels, val_labels, test_labels
 
