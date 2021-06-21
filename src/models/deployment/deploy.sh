199f6939cc5c32b6db22f94d6058cca71f764a9f
Rasmus Aagaard <raagaard97@gmail.com>
2021-06-21 18:33:21 +0200

    implemented deployment

---
+++ src/models/deployment/deploy.sh
@@ -0,0 +1,2 @@
+torch-model-archiver -f --model-name "FakeNews" --version 1.0 --serialized-file ./models/roberta_fakenews-final/pytorch_model.bin --extra-files "./models/roberta_fakenews-final/config.json,./src/models/deployment/index_to_name.json" --handler "./src/models/deployment/handler.py" --export-path "./models/" && \
+torchserve --start --model-store models --models FakeNews=FakeNews.mar --ncs
