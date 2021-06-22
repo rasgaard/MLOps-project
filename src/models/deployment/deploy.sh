d3414f3ee9289afc79f4997971acebb6d3992b28
Rasmus Aagaard <raagaard97@gmail.com>
2021-06-22 18:52:43 +0200

    train_model saves best epoch

--- src/models/deployment/deploy.sh
+++ src/models/deployment/deploy.sh
@@ -1,2 +1,7 @@
-torch-model-archiver -f --model-name "FakeNews" --version 1.0 --serialized-file ./models/roberta_fakenews-final/pytorch_model.bin --extra-files "./models/roberta_fakenews-final/config.json,./src/models/deployment/index_to_name.json" --handler "./src/models/deployment/handler.py" --export-path "./models/" && \
+torch-model-archiver -f --model-name "FakeNews" \
+                        --version 1.0 \
+                        --serialized-file ./models/roberta_fakenews-final/pytorch_model.bin \
+                        --extra-files "./models/roberta_fakenews-final/config.json,./src/models/deployment/index_to_name.json" \
+                        --handler "./src/models/deployment/handler.py" \
+                        --export-path "./models/" && \
 torchserve --start --model-store models --models FakeNews=FakeNews.mar --ncs
