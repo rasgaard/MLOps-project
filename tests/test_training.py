470be2c21a977c331b85cde1e01d60f02b2b441a
sofiebach <70587917+sofiebach@users.noreply.github.com>
2021-06-23 08:56:50 +0200

    test training

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,90 +1,88 @@
-import argparse
-import sys
-
 import torch
 from torch.utils.data import DataLoader
-from transformers import AdamW, RobertaForSequenceClassification
+from transformers import AdamW, RobertaForSequenceClassification, get_scheduler
 
 from src.data.make_dataset import read_data
 from src.features.build_features import encode_texts
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device(
+    'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-def test_training_evaluation(epochs=1, lr=0.003):
-    model = RobertaForSequenceClassification.from_pretrained('roberta-base', output_hidden_states=True)
+
+def test_training_evaluation(epochs=1, lr=0.00005):
+    model = RobertaForSequenceClassification.from_pretrained(
+        'roberta-base', output_hidden_states=True)
     model.to(device)
     model.train()
 
-    optim = AdamW(model.parameters(), lr=lr)
+    optimizer = AdamW(model.parameters(), lr=lr)
     train_texts, val_texts, _, train_labels, val_labels, _ = read_data()
-    train_texts, val_texts, train_labels, val_labels = train_texts[:3], val_texts[:3], train_labels[:3], val_labels[:3]
-    encoded_train, encoded_val = encode_texts(train_texts, train_labels), encode_texts(val_texts, val_labels)
-    train_loader = DataLoader(encoded_train, 3, shuffle=True)
-    val_loader = DataLoader(encoded_val, 3, shuffle=True)
+    train_texts, val_texts, train_labels, val_labels = train_texts[
+        :6], val_texts[:6], train_labels[:6], val_labels[:6]
+    encoded_train, encoded_val = encode_texts(
+        train_texts, train_labels), encode_texts(val_texts, val_labels)
+    train_loader = DataLoader(encoded_train, 2, shuffle=True)
+    val_loader = DataLoader(encoded_val, 2, shuffle=True)
+
+    num_training_steps = epochs * len(train_loader)
+    lr_scheduler = get_scheduler(
+        "linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps
+    )
 
     for epoch in range(epochs):
         weights = []
-        running_loss_train = 0
-        running_loss_val = 0
-        for batch_idx, batch in enumerate(train_loader):
-            optim.zero_grad()
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-            loss = outputs[0]
-            running_loss_train += loss
+        weights_eval = []
+        running_train_loss = 0
+        model.train()
+        for batch in train_loader:
+            # puts tensors in batch to device
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            loss = outputs.loss
             loss.backward()
-            optim.step()
+
+            running_train_loss += loss.item()
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
 
             w = []
-            for name, param in model.named_parameters():
+            for _, param in model.named_parameters():
                 w.append(param)
             weights.append(w)
 
-
-            print(f"\nTrain batch {batch_idx+1}/{len(train_loader)}\n", end='\r')
-    
-
-        # Check that at least one weight changes after running each batch
+        # Check that the weights are updated after each batch
         check_weights = []
         for i in range(len(weights)-1):
             for j in range(len(weights[i])):
-                #print(weights[i][j].shape)
-                #print(weights[i+1][j].shape)
                 check_weights.append(weights[i][j] != weights[i+1][j])
-            #print(check_weights[i])
             check_weights[i] = sum(sum(check_weights[i]))
-            print(check_weights[i])
-            #print(sum(sum(check_weights)))
-            
-            #assert sum(sum(sum(weights[i] != weights[i+1]))) > 0
+            # should work on larger amount of training data / non-pretrained model
+            # assert check_weights[i].item() > 0
 
-        weights_eval = []
-        with torch.no_grad():
-            model.eval()
-            for batch_idx, batch in enumerate(val_loader):
-                input_ids = batch['input_ids'].to(device)
-                attention_mask = batch['attention_mask'].to(device)
-                labels = batch['labels'].to(device)
+        model.eval()
+        for batch in val_loader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            with torch.no_grad():
+                outputs = model(**batch)
 
-                outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-                loss = outputs[0]
+                w_eval = []
+                for _, param in model.named_parameters():
+                    w_eval.append(param)
+                weights_eval.append(w_eval)
 
-                weights_eval.append(outputs[2][1])
-
-                running_loss_val += loss
-                print(f"\nValidation batch {batch_idx+1}/{len(train_loader)}\n", end='\r')
-        
-        # Check that the weights don't change in eval
-        #for i in range(len(weights_eval)-1):
-            #print(weights_eval[i])
-            #print(weights_eval[i+1])
-            #print(sum(sum(sum(weights_eval[i] == weights_eval[i+1]))))
-            #assert sum(sum(sum(weights_eval[i] == weights_eval[i+1]))) > 0
+        # Check that the weights are not updated when evaluating
+        check_weights_eval = []
+        for i in range(len(weights_eval)-1):
+            for j in range(len(weights_eval[i])):
+                check_weights_eval.append(
+                    weights_eval[i][j] != weights_eval[i+1][j])
+            check_weights_eval[i] = sum(sum(check_weights_eval[i]))
+            assert check_weights_eval[i].item() == 0
 
 
-        
-    
 test_training_evaluation()
